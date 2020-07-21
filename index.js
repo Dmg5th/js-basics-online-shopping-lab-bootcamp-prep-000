f8e7149e526fbdc93f7372249f4e57582b1d290e
Daniel Gould <dmgould310@gmail.com>
2020-07-21 13:55:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 var cart = [];
 
-function getCart() {
- return cart;
-}
-
-function setCart(c) {
-  cart = c;
-  return cart;
-}
 
 function addToCart(item) {
- // write your code here
+  const price = Math.floor(Math.random() * 100)
+
+  cart.push({ [item]: price })
+
+  console.log(`${item} has been added to your cart.`)
+
+  return cart
 }
 
-function viewCart() {
-  // write your code here
-}
 
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
